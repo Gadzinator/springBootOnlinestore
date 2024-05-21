0b74fc9378354b334541d72682c9ab89658bd2e9
grigorev <ilovemeet2022@gmail.com>
2024-05-21 21:21:08 +0300

    Added ProductDto

---
+++ main/src/main/java/com/springboot/onlinestore/domain/dto/ProductDto.java
@@ -0,0 +1,46 @@
+package com.springboot.onlinestore.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.springboot.onlinestore.utils.DateConstant;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+	private long id;
+
+	@NotBlank(message = "name should not be blank")
+	@Size(max = 30, message = "name length should not exceed 255 characters")
+	private String name;
+
+	@NotBlank(message = "brand should not be blank")
+	@Size(max = 30, message = "brand length should not exceed 255 characters")
+	private String brand;
+
+	@NotBlank(message = "description should not be blank")
+	@Size(max = 255, message = "description length should not exceed 255 characters")
+	private String description;
+
+	@NotBlank(message = "category should not be blank")
+	private String category;
+
+	@Min(value = 0)
+	private int price;
+
+	@JsonFormat(pattern = DateConstant.DEFAULT_DATE_PATTERN)
+	@NotBlank(message = "created should not be blank")
+	private String created;
+
+	@NotBlank(message = "isAvailable should not be blank")
+	@JsonProperty("is_available")
+	private boolean isAvailable;
+
+	@NotBlank(message = "received should not be blank")
+	@JsonFormat(pattern = DateConstant.DEFAULT_DATE_PATTERN)
+	private String received;
+}
---
+++ main/src/main/java/com/springboot/onlinestore/utils/DateConstant.java
@@ -0,0 +1,6 @@
+package com.springboot.onlinestore.utils;
+
+public class DateConstant {
+
+	public static final String DEFAULT_DATE_PATTERN = "dd-MM-yyyy";
+}
