d2f9d192c12f85fac977d49412bd7a08214ee651
grigorev <ilovemeet2022@gmail.com>
2024-05-21 22:08:02 +0300

    Added PasswordChangeRequest

---
+++ main/src/main/java/com/springboot/onlinestore/domain/dto/PasswordChangeRequest.java
@@ -0,0 +1,16 @@
+package com.springboot.onlinestore.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PasswordChangeRequest {
+
+	@NotBlank(message = "oldPassword should not be blank")
+	private String oldPassword;
+
+	@NotBlank(message = "newPassword should not be blank")
+	@Size(min = 6, max = 20, message = "newPassword length should be between 6 and 20 characters")
+	private String newPassword;
+}
